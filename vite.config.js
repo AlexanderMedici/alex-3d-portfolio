f9eb336063d186814fd38af085f7b1e3b7bbf87b
Alexander M <hellomedici@outlook.com>
2026-01-02 11:11:33 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: parseInt(process.env.PORT) || 5173,
-    allowedHosts: [
-      "alex-3d-portfolio.onrender.com",
-      "alexmedici.online",
-      "www.alexmedici.online",
-    ],
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: parseInt(process.env.PORT) || 4173,
-  },
+  base: "/", // IMPORTANT
 });
